e0506f49080734ba1c54aeeefc6f3e8dd886e989
tmenture <thomasoxemail@gmail.com>
2022-04-28 14:10:00 -0400

    started adding questions, and installed necessary packages

--- index.js
+++ index.js
@@ -1,7 +1,56 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = reduire('util');
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project titled?"
+    },
+
+    {
+        type: "input",
+        name: "description",
+        message: "Give a breif description of your project."
+    },
+
+    {
+        type: "input",
+        name: "instalations",
+        message: "Are there any instalations required to run the project?"
+    },
+
+    {
+        type: "input",
+        name: "usage",
+        message: "What is the use of this application?"
+    },
+
+    {
+        type: "input",
+        name: "contributions",
+        message: "Are there any contributions to the project?"
+    },
+
+    {
+        type: "input",
+        name: "testing",
+        message: "Please provide testing instructions if necessary."
+    },
+
+    {
+        type: "checkbox",
+        name: "license",
+        message: "Are there any license's?",
+        choices: ["MIT License", "GNU GPLv3 License"]
+    },
+
+
+];
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
