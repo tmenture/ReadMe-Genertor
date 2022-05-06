11abb496bc4bd17fbbdffd40f3d980bc6a934d38
tmenture <thomasoxemail@gmail.com>
2022-05-06 02:20:38 -0400

    everything is in working order

--- develop/utils/renderLicense.js
+++ develop/utils/renderLicense.js
@@ -15,7 +15,6 @@ function renderLicense(value) {
     } else {
         return "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
     } 
-    
 }
 
 module.exports = {
